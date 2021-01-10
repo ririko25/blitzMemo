66566a92ce9a33b3b3da7841b3570395acc22619
ririko <ririmohu25@gmail.com>
2021-01-10 16:14:26 +0900

    seed作成

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,4 +1,5 @@
-// import db from "./index"
+import { hashPassword } from "app/auth/auth-utils"
+import db from "./index"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -7,10 +8,37 @@
  * or https://github.com/Marak/Faker.js to easily generate
  * realistic data.
  */
+
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  const hashedPassword = await hashPassword("password!!")
+  const email = "hoge@example.com".toLowerCase()
+  const userId = (
+    await db.user.create({
+      data: { email, hashedPassword, role: "user" },
+      select: { id: true, name: true, email: true, role: true },
+    })
+  ).id
+
+  await db.category.create({
+    data: {
+      name: "カテゴリー１",
+      memos: {
+        create: [
+          {
+            title: "メモ１",
+            body: "メモの１つ目です。",
+            user: { connect: { id: userId } },
+          },
+          {
+            title: "メモ２",
+            body: "メモの２つ目です。",
+            user: { connect: { id: userId } },
+          },
+        ],
+      },
+      user: { connect: { id: userId } },
+    },
+  })
 }
 
 export default seed
